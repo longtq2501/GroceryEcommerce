e19ee1f579180c5f59a0bb9bf0e37a6b61670130
Ton Quynh Long <longxp03@gmail.com>
2024-09-06 23:17:40 +0700

    header #1

--- front-end/src/routes/RouterProcess.jsx
+++ front-end/src/routes/RouterProcess.jsx
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "../pages/AuthenticateService/Login/Login";
+import Home from "../pages/Home/Home";
 
 const RouterProcess = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home  />}/>
       </Routes>
     </Router>
   );
